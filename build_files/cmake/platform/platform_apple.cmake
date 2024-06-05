481121c1d35abbae03488b26f039f4ffed5415cf
Mark Bolstad <the.render.dude@gmail.com>
2024-06-05 15:19:02 -0600

    Distributed: Find MPI and ZeroMQ packages
    Add cmake option to build for distributed rendering and finding MPI and ZeroMQ

--- build_files/cmake/platform/platform_apple.cmake
+++ build_files/cmake/platform/platform_apple.cmake
@@ -340,6 +340,29 @@ if(WITH_CYCLES AND WITH_CYCLES_EMBREE)
 endif()
 add_bundled_libraries(embree/lib)
 
+if(WITH_CYCLES AND WITH_CYCLES_DISTRIBUTED)
+  find_package(MPI REQUIRED)
+  message(STATUS "Found MPI: ${MPI_C_LIBRARIES}")
+
+  ## load in pkg-config support
+  find_package(PkgConfig)
+  ## use pkg-config to get hints for 0mq locations
+  pkg_check_modules(PC_ZeroMQ QUIET zmq)
+
+  ## use the hint from above to find where 'zmq.hpp' is located
+  find_path(ZeroMQ_INCLUDE_DIR
+          NAMES zmq.hpp
+          PATHS ${PC_ZeroMQ_INCLUDE_DIRS}
+          )
+
+  ## use the hint from above to find the location of libzmq
+  find_library(ZeroMQ_LIBRARY
+          NAMES zmq
+          PATHS ${PC_ZeroMQ_LIBRARY_DIRS}
+          )
+  message(STATUS "Found ZeroMQ: ${ZeroMQ_LIBRARY}")
+endif()
+
 if(WITH_OPENIMAGEDENOISE)
   find_package(OpenImageDenoise REQUIRED)
   add_bundled_libraries(openimagedenoise/lib)
