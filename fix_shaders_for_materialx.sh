236dc5c333d8f701b58df71b7e57e3f57b7fb0ef
Mark Bolstad <the.render.dude@gmail.com>
2024-10-15 17:18:22 -0600

    Fix MaterialX for RenderMan mode
    Several fixes for incorporating MaterialX into RenderMan mode:
     - Remove FindMaterialX as it was hosing the Blender usage of MaterialX and rely on the path to the config file
     - Remove the shell script for ensuring MaterialX (and Pixar) shaders were in a known location (only worked for XCode builds)
     - Add cmake custom command to replace the functionality of the shell script

--- fix_shaders_for_materialx.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-pushd ../build_xcode_cycles/bin/Debug/
-ln -s ../../intern/cycles/kernel/osl/shaders shader
-popd
-cp lib/macos_arm64/materialx/libraries/stdlib/genosl/include/*h ../build_xcode_cycles/bin/Debug/shader
-cp ./intern/cycles/kernel/osl/shaders/stdcycles.h ../build_xcode_cycles/bin/Debug/shader
-cp lib/macos_arm64/osl/share/OSL/shaders/stdosl.h ../build_xcode_cycles/bin/Debug/shader
