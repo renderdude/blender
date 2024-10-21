328dcaddd05612b34b88dad4f6be921e988af18d
Mark Bolstad <the.render.dude@gmail.com>
2024-10-21 12:07:46 -0600

    Update build system for distributed mode
    The funky code in 'platform_apple' is to workaround issues with ZeroMQ inclusion pointing to the default homebrew location, which causes issues with duplicate TBB libraries.
    The blender way would be to include ZeroMQ and cppzmq into extern. Maybe later

--- build_files/cmake/platform/platform_apple.cmake
+++ build_files/cmake/platform/platform_apple.cmake
@@ -347,23 +347,16 @@ if(WITH_CYCLES AND WITH_CYCLES_DISTRIBUTED)
   find_package(MPI REQUIRED)
   message(STATUS "Found MPI: ${MPI_C_LIBRARIES}")
 
-  ## load in pkg-config support
-  find_package(PkgConfig)
-  ## use pkg-config to get hints for 0mq locations
-  pkg_check_modules(PC_ZeroMQ QUIET zmq)
-
-  ## use the hint from above to find where 'zmq.hpp' is located
-  find_path(ZeroMQ_INCLUDE_DIR
-          NAMES zmq.hpp
-          PATHS ${PC_ZeroMQ_INCLUDE_DIRS}
-          )
+  find_package(cppzmq REQUIRED)
+  if (IS_SYMLINK "${cppzmq_INCLUDE_DIR}/zmq.hpp")
+    FILE(REAL_PATH "${cppzmq_INCLUDE_DIR}/zmq.hpp" foo)
+    cmake_path(GET foo PARENT_PATH cppzmq_INCLUDE_DIR)
+    FILE(REAL_PATH "${ZeroMQ_INCLUDE_DIR}/zmq.h" foo)
+    cmake_path(GET foo PARENT_PATH ZeroMQ_INCLUDE_DIR)
+    FILE(REAL_PATH "${ZeroMQ_LIBRARY}" ZeroMQ_LIBRARY)
+  endif()
+  message(STATUS "Found cppzmq includes: ${cppzmq_INCLUDE_DIR}")
   message(STATUS "Found ZeroMQ includes: ${ZeroMQ_INCLUDE_DIR}")
-
-  ## use the hint from above to find the location of libzmq
-  find_library(ZeroMQ_LIBRARY
-          NAMES zmq
-          PATHS ${PC_ZeroMQ_LIBRARY_DIRS}
-          )
   message(STATUS "Found ZeroMQ: ${ZeroMQ_LIBRARY}")
 endif()
 
